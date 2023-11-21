62b4262f8d1c2e3aafd9fa4d4442598c9c929e5d
SrTime <72728663+SrTime@users.noreply.github.com>
2023-11-21 16:30:09 +0900

    -image 폴더에 사진 추가 -Card클래스에서 image 폴더안에 '동물', '풍경' 폴더 중에서 랜덤으로 선택하여 deck에 저장하도록 변경 -GFrame 클래스 일부 변경

--- GFrame.java
+++ GFrame.java
@@ -3,15 +3,13 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Image; // 추가
+import java.awt.Image;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.border.LineBorder;
-
-
 import javax.swing.ImageIcon;
 
 public class GFrame extends JFrame {
@@ -22,55 +20,47 @@ public class GFrame extends JFrame {
     private int clickCount;
 
     public GFrame(List<Card> deck) {
-
         player1 = new Player("Player1");
         player2 = new Player("Player2");
-        currentPlayer = player1; // 처음에는 player1이 선택
+        currentPlayer = player1; 
         clickCount = 0;
         Font font1 = new Font("Showcard Gothic", Font.BOLD, 30);
 
-        // GUI 설정
         setLayout(new GridLayout(3, 3));
         setSize(900, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // 카드 버튼 생성
         buttons = new ArrayList<>();
         for (int i = 0; i < deck.size(); i++) {
             int finalI = i;
-            JButton button = new JButton("?"); // 버튼의 초기 텍스트를 "?"로 설정
-            button.setBorder(new LineBorder(Color.WHITE)); // 초기 테두리 색상은 하얀색
+            JButton button = new JButton("?");
+            button.setBorder(new LineBorder(Color.WHITE)); 
 
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    // 버튼을 클릭하면 카드를 뽑고 테두리 색상을 변경
                     String fileName = deck.get(finalI).getImage();
-                    String baseName = fileName.substring(0, fileName.lastIndexOf('.')); // 확장자 제거
-                    button.setFont(font1);
-                    button.setText(baseName); // 버튼의 텍스트를 해당 카드의 파일 이름으로 변경(확장자 제외)
-                    currentPlayer.drawCard(deck.get(finalI)); // 현재 플레이어에 카드 정보 전달
-                    File imageFile = new File("./images/" + fileName);
-                    ImageIcon icon = new ImageIcon(imageFile.getAbsolutePath()); // 이미지 아이콘 생성
-                    Image image = icon.getImage(); // ImageIcon에서 Image 추출
-        Image resizedImage = image.getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH); // 이미지 크기 조정
-        button.setIcon(new ImageIcon(resizedImage));
+                    ImageIcon icon = new ImageIcon(fileName);
+                    Image image = icon.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+                    button.setIcon(new ImageIcon(image));
+
+                    currentPlayer.drawCard(deck.get(finalI)); 
+
                     if (currentPlayer == player1) {
-                        button.setBorder(new LineBorder(Color.BLUE, 10)); // player1의 턴일 때는 파란색 테두리
-                        currentPlayer = player2; // player를 변경
+                        button.setBorder(new LineBorder(Color.BLUE, 10)); 
+                        currentPlayer = player2; 
                     } else {
-                        button.setBorder(new LineBorder(Color.RED, 10)); // player2의 턴일 때는 빨간색 테두리
-                        currentPlayer = player1; // player를 변경
+                        button.setBorder(new LineBorder(Color.RED, 10)); 
+                        currentPlayer = player1; 
                     }
-                    button.setEnabled(false); // 클릭한 버튼은 비활성화
+                    button.setEnabled(false); 
                     clickCount++;
-                    if (clickCount == 2) { // 두 번 클릭되면 모든 버튼 비활성화
+                    if (clickCount == 2) { 
                         for (JButton btn : buttons) {
                             btn.setEnabled(false);
                         }
                     }
                 }
-
             });
             buttons.add(button);
             add(button);
