f537adc9f65e0b6cb22b330c85ed89a332af6e87
JIGSAW <bm8383@naver.com>
2023-11-21 17:36:13 +0900

    버튼 기능 최종 수정 및 그림 사이즈 고정

--- GFrame.java
+++ GFrame.java
@@ -7,6 +7,7 @@ import java.awt.Point;  // Point 클래스를 import
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -89,37 +90,35 @@ public class GFrame extends JFrame {
     }
 }
 
-
 class NextFrame extends JFrame {
     private Player player1;
     private Player player2;
-    private int visibleWidth = 0;  
+    private int visibleWidth = 0;
     private int visibleHeight = 0;
 
     public NextFrame(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
 
-        setSize(800, 800);  
+        setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
         // 첫 번째 플레이어의 마지막 카드를 가져옴
         Card lastCard1 = player1.getLastCard();
         // 첫 번째 플레이어의 마지막 카드 이미지를 레이블로 생성
-        ImageIcon imageIcon = new ImageIcon("./images/" + lastCard1.getImage());
-        int imageWidth = imageIcon.getIconWidth();
-        int imageHeight = imageIcon.getIconHeight();
+        ImageIcon originalIcon = new ImageIcon("./images/" + lastCard1.getImage());
+        ImageIcon scaledIcon = new ImageIcon(originalIcon.getImage().getScaledInstance(500, 500, Image.SCALE_DEFAULT));
 
         // Custom label that shows only a portion of the image
-        JLabel label = new JLabel(imageIcon) {
+        JLabel label = new JLabel(scaledIcon) {
             @Override
             protected void paintComponent(Graphics g) {
                 g.setClip(0, 0, visibleWidth, visibleHeight);
                 super.paintComponent(g);
             }
         };
-        label.setBounds(0, 0, imageWidth, imageHeight);
+        label.setBounds(0, 0, 500, 500);
         getContentPane().add(label);
 
         // 4방향 버튼
@@ -141,7 +140,7 @@ class NextFrame extends JFrame {
         rightButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (visibleWidth < imageWidth) {
+                if (visibleWidth < 500) {
                     visibleWidth += 30;
                     label.repaint();
                 }
@@ -167,7 +166,7 @@ class NextFrame extends JFrame {
         downButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (visibleHeight < imageHeight) {
+                if (visibleHeight < 500) {
                     visibleHeight += 30;
                     label.repaint();
                 }
@@ -175,6 +174,16 @@ class NextFrame extends JFrame {
         });
         getContentPane().add(downButton);
 
+        JButton revealButton = new JButton("한번에 공개");
+        revealButton.setBounds(500,400,150,50);
+        revealButton.addActionListener(e -> {
+            visibleWidth = 500;
+            visibleHeight = 500;
+            label.repaint();
+        });
+        getContentPane().add(revealButton);
+
         setVisible(true);
     }
 }
+
