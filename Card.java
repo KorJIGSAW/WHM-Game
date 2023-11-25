2dcac62348cf0b3f8b065ec8b49e627ab979f8e1
SrTime <72728663+SrTime@users.noreply.github.com>
2023-11-25 19:10:05 +0900

    - RandomCard 수정중

--- Card.java
+++ Card.java
@@ -1,10 +1,10 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 class Card {
     private String image;
@@ -28,36 +28,26 @@ class Card {
     public int getPeopleCount() {
         return peopleCount;
     }
-    
+
     public static List<Card> insert_card() {
-    List<String> imageNames = new ArrayList<>();
-    try {
-        imageNames = Files.readAllLines(Paths.get("imageNames.txt"));
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    List<Card> deck = new ArrayList<>();
-    for (String name : imageNames) {
-        File file = new File("./image/나무/" + name);
-        if (!file.exists()) {
-            file = new File("./image/동물/" + name);
-            if (!file.exists()) {
-                file = new File("./image/사람/" + name);
+        List<String> imageNames = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("RandomCard.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                imageNames.add(line.trim());
             }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if (file.exists()) {
-            deck.add(new Card(file.getPath(), false, 103)); // peopleCount는 예시입니다. 실제로는 적절한 값을 사용해주세요.
-        }
-    }
-    return deck;
-}
-}
 
-/*
- * 고쳐야할 부분이 있다. 
- * 풍경, 동물, 사람 폴더에 있는 이미지를 랜덤으로 뽑아서 카드를 만들어야 한다.
- * 현재 코드를 보면 아래쪽에 Boolean형태로 갈라서 둘중 하나로 넣고 있는 image/사람 폴더에 사진 9개를 추가하고
- * 3폴더에서 총 27장중 9장을 랜덤으로 선별하는 코드가 필요하다.
- * 이 내용은 수정시 주석을 삭제한다.
- */
+        Collections.shuffle(imageNames);
+
+        List<Card> deck = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            deck.add(new Card(imageNames.get(i), false, 0));
+        }
+        return deck;
+    }
+}
--- GFrame.java
+++ GFrame.java
@@ -4,13 +4,9 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-// GFrame.java
 public class GFrame extends JFrame {
     private Player player1;
     private Player player2;
@@ -19,22 +15,11 @@ public class GFrame extends JFrame {
     private int clickCount;
 
     public GFrame(Theme theme) {
-    List<Card> deck = new ArrayList<>();
-    try {
-        // 이미지 파일 이름을 읽습니다.
-        List<String> lines = Files.readAllLines(Paths.get("imageNames.txt"));
+        List<Card> deck = Card.insert_card();
 
-        // 각 줄에 대해 Card 객체를 생성하고 deck에 추가합니다.
-        for (String line : lines) {
-            deck.add(new Card(line.trim(), false, 0)); // 모든 카드를 뒷면으로 설정하고, peopleCount를 0으로 설정합니다.
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    
         player1 = new Player("Player1");
         player2 = new Player("Player2");
-        currentPlayer = player1; 
+        currentPlayer = player1;
         clickCount = 0;
         Font font1 = new Font("Showcard Gothic", Font.BOLD, 30);
 
@@ -46,7 +31,7 @@ public class GFrame extends JFrame {
         for (int i = 0; i < deck.size(); i++) {
             int finalI = i;
             JButton button = new JButton("?");
-            button.setBorder(new LineBorder(Color.WHITE)); 
+            button.setBorder(new LineBorder(Color.WHITE));
 
             button.addActionListener(new ActionListener() {
                 @Override
@@ -56,22 +41,22 @@ public class GFrame extends JFrame {
                     Image image = icon.getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH);
                     button.setIcon(new ImageIcon(image));
 
-                    currentPlayer.drawCard(deck.get(finalI)); 
+                    currentPlayer.drawCard(deck.get(finalI));
 
                     if (currentPlayer == player1) {
-                        button.setBorder(new LineBorder(Color.BLUE, 10)); 
-                        currentPlayer = player2; 
+                        button.setBorder(new LineBorder(Color.BLUE, 10));
+                        currentPlayer = player2;
                     } else {
-                        button.setBorder(new LineBorder(Color.RED, 10)); 
-                        currentPlayer = player1; 
+                        button.setBorder(new LineBorder(Color.RED, 10));
+                        currentPlayer = player1;
                     }
-                    button.setEnabled(false); 
+                    button.setEnabled(false);
                     clickCount++;
-                    if (clickCount == 2) { 
+                    if (clickCount == 2) {
                         for (JButton btn : buttons) {
                             btn.setEnabled(false);
                         }
-                        new OpenCard(player1, player2); // OpenCard 실행
+                        new OpenCard(player1, player2);
                     }
                 }
             });
